f6337c55b43b5ec89e7bd18af9c073755c64ea2c
Witek <koniokrad7@gmail.com>
2015-05-02 21:44:54 +0200

    commit y

--- WindowsFormsApplicationTest/Program.cs
+++ WindowsFormsApplicationTest/Program.cs
@@ -13,7 +13,7 @@ namespace WindowsFormsApplicationTest
         /// </summary>
         [STAThread]
         static void Main()
-        {   //test1
+        {   //test1   lll
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
